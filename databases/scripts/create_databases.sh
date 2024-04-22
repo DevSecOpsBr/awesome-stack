0497c4f0ee2f9aa4547fa152357e80f50ce03834
Rodrigo Carvalho <rdgacarvalho@gmail.com>
2024-04-22 21:36:20 +0200

    db: add gitness database
    Signed-off-by: Rodrigo Carvalho <rdgacarvalho@gmail.com>

---
+++ databases/scripts/create_databases.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER drone_bb;
+	CREATE DATABASE drone_bitbucket;
+	GRANT ALL PRIVILEGES ON DATABASE drone_bitbucket TO drone_bb;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER drone_gh;
+	CREATE DATABASE drone_github;
+	GRANT ALL PRIVILEGES ON DATABASE drone_github TO drone_gh;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER gitness;
+	CREATE DATABASE gitness;
+	GRANT ALL PRIVILEGES ON DATABASE gitness TO gitness;
+EOSQL
